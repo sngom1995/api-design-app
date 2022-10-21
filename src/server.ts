e64593e2d2768707849d7ed6cc591194889831b8
sngom@solid-afrique.com <sngom@solid-afrique.com>
2022-10-21 16:24:01 +0000

    add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import cors from 'cors'
 
 const app = express();
 
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.get('/', (req, res)=>{
     console.log("listen from server");
