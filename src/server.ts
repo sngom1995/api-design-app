2a266bc993e4c614b4de896d44ad1d34d0aed147
sngom@solid-afrique.com <sngom@solid-afrique.com>
2022-10-21 15:56:22 +0000

    routes designing

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
-
+import router from './router';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res)=>{
     console.log("listen from server");
     res.status(200);
     res.json({message: "hello"});
 })
+
+app.use('/api', router)
 
 export default app;
